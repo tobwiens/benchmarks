408d00410b9e8ab830c146430ea321363e7e9b31
Tobias Wiens <tobwiens@gmail.com>
2015-05-05 09:35:54 -0400

    COrrect path

--- prepBenchmark.sh
+++ prepBenchmark.sh
@@ -5,7 +5,7 @@ echo 'Clone benchmark repository, ssh key must be configured'
 git clone git@github.com:tobwiens/benchmarks.git
 
 # Install dependencies
-cat benchmark/installDependencies/sh | sh
+cat benchmarks/installDependencies/sh | sh
 
 echo "Enter save correct IP address in MY_IP environment variable" 
 echo "Make sure that node.list is up to date"
