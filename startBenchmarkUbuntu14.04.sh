85aaffb8618a9ace6be57e3d995d65e325978516
Tobias Wiens <tobwiens@gmail.com>
2015-05-05 09:01:24 -0400

    Use IP as foldername

--- startBenchmarkUbuntu14.04.sh
+++ startBenchmarkUbuntu14.04.sh
@@ -2,18 +2,18 @@
 
 # Create unique folder name
 folderName=`date +%Y.%m.%d-%H:%M:%S`
-
-# Create folder for logs
-mkdir logs/$folderName
+# Clean logs and create folder for logs
+rm -rf logs
+mkdir logs
 
 # Start stream benchmark 
-stream/runStreamUbuntu14.04.sh $folderName stream/ubuntu14.04x64
+stream/runStreamUbuntu14.04.sh $MY_IP  stream/ubuntu14.04x64
 
 # Start LINPACK benchmark
 
 # Commit logs
 
 # Push logs:wqq
-git add logs/$folderName
-git commit -m "Current benchmark $folderName"
+git add logs/
+git commit -m "Current benchmark $folderName from $MY_IP"
 git push	
